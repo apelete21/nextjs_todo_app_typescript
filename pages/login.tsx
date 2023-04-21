66da4b6e28d3f4c0348e53323cdb3e6b2cb647d6
CoderZ <adzohonouapelete@gmail.com>
2023-04-21 16:43:32 +0000

    login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
-const inputStyle = "w-full my-2 rounded-lg p-2 placeholder-white";
-const buttonStyle = "dark:bg-blue-600 bg-blue-300 cursor-pointer hover:opacity-60";
+const inputStyle = "w-full my-2 rounded-lg p-2";
+const buttonStyle =
+  "dark:bg-violet-800 bg-blue-600 dark:text-white text-white cursor-pointer hover:opacity-60";
 
 export default function Home() {
   return (
     <>
       <div className="w-screen h-screen flex flex-col items-center justify-center">
-        <div className="w-[450px] h-auto bg text-center flex flex-col items-center p-4 justify-center rounded-2xl bg-[#dddd] dark:bg-[#5555]">
-          <h1 className="w-full text-4xl my-[20px]">Todo App</h1>
+        <div className="w-[450px] h-auto bg text-center flex flex-col items-center p-4 justify-center rounded-2xl bg-gray-300 dark:bg-[#333]">
+          <h1 className="w-full text-4xl font-bold my-[20px]">Todo App</h1>
           <h3 className="w-full mb-6 opacity-80">
             Welcome, enter your credentials
           </h3>
@@ -16,19 +17,19 @@ export default function Home() {
           <input
             type="email"
             placeholder="Email"
-            className={`${inputStyle}  bg-[#dddd] dark:bg-[#5555]`}
+            className={`${inputStyle}  bg-gray-100 dark:bg-[#5555]`}
           />
           <input
             type="password"
             placeholder="Password"
-            className={`${inputStyle}  bg-[#dddd] dark:bg-[#5555]`}
+            className={`${inputStyle}  bg-gray-100 dark:bg-[#5555]`}
           />
           <input
             type="submit"
             value={"Login"}
             className={`${inputStyle} ${buttonStyle}`}
           />
-          <Link href="#" className="text-[12px] text-right w-full">
+          <Link href="#" className="text-[12px] text-right font-black w-full">
             Forgot your password ?
           </Link>
 

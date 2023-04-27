02d90a3322357b5a602c1ccc58b5259f511ac172
CoderZ <adzohonouapelete@gmail.com>
2023-04-27 19:32:06 +0000

    [user].tsx deleted & added conditionnal render on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import { Welcome } from "@/components";
+import { App, Welcome } from "@/components";
+import { UserContext } from "@/contexts";
+import { useContext } from "react";
 
 export default function Home(): JSX.Element {
-  return (
-    <>
-     <Welcome />
-    </>
-  );
+  const {
+    isLoading,
+    sessionSet,
+    currentUser,
+    setIsLoading,
+    setSessionSet,
+    setCurrentUser,
+  }: any = useContext(UserContext);
+
+  if (sessionSet)
+    return (
+      <>
+        <App />
+      </>
+    );
+  else
+    return (
+      <>
+        <Welcome />
+      </>
+    );
 }
